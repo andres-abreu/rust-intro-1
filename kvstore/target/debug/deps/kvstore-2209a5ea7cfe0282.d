108d482e92a8b7fd5600938d62bc8b41189f2782
Andres Abreu <andresabreu@andress-MacBook-Pro.local>
2022-09-19 12:39:53 -0400

    rust-build initiated

---
+++ kvstore/target/debug/deps/kvstore-2209a5ea7cfe0282.d
@@ -0,0 +1,5 @@
+/Users/andresabreu/Desktop/rust-intro-1/kvstore/target/debug/deps/kvstore-2209a5ea7cfe0282: src/main.rs
+
+/Users/andresabreu/Desktop/rust-intro-1/kvstore/target/debug/deps/kvstore-2209a5ea7cfe0282.d: src/main.rs
+
+src/main.rs:
---
+++ kvstore/target/debug/kvstore.d
@@ -0,0 +1,1 @@
+/Users/andresabreu/Desktop/rust-intro-1/kvstore/target/debug/kvstore: /Users/andresabreu/Desktop/rust-intro-1/kvstore/src/main.rs
