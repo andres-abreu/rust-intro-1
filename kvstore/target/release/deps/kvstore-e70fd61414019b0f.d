7f5bc5a7f42d9edeba81defe643df8e86858b75f
Andres Abreu <andresabreu@andress-MacBook-Pro.local>
2022-09-19 12:43:45 -0400

    release was built

---
+++ kvstore/target/release/deps/kvstore-e70fd61414019b0f.d
@@ -0,0 +1,5 @@
+/Users/andresabreu/Desktop/rust-intro-1/kvstore/target/release/deps/kvstore-e70fd61414019b0f: src/main.rs
+
+/Users/andresabreu/Desktop/rust-intro-1/kvstore/target/release/deps/kvstore-e70fd61414019b0f.d: src/main.rs
+
+src/main.rs:
---
+++ kvstore/target/release/kvstore.d
@@ -0,0 +1,1 @@
+/Users/andresabreu/Desktop/rust-intro-1/kvstore/target/release/kvstore: /Users/andresabreu/Desktop/rust-intro-1/kvstore/src/main.rs
